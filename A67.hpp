b7208da73c04a783bc6e787df7625840030e8952
Pardeep Klair <106414050+pardeepklair@users.noreply.github.com>
2022-10-12 21:43:53 -0700

    Add files via upload

--- A67.hpp
+++ A67.hpp
@@ -17,33 +17,7 @@ void swap(int &, int &, int &);
 void swap(int &, int &, int &, int &);
 
 
-int main() 
-{
-    int num;
-    int val1, val2, val3, val4;
 
-    do{
-        getinput(num);
-        switch(num) {
-            case 2:
-                getinput(val1, val2);
-                swap(val1, val2);
-                break;
-            case 3:
-                getinput(val1, val2, val3);
-                swap(val1, val2, val3);
-                break;
-            case 4:
-                getinput(val1, val2, val3, val4);
-                swap(val1, val2, val3, val4);
-                break;
-            default: 
-                break;
-        }
-
-    }while(num != 1);
-
-}
 
 
 // Functions
