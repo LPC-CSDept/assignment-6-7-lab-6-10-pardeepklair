30311b849ba976e587991cfcac3f8191013bc7bc
Pardeep Klair <106414050+pardeepklair@users.noreply.github.com>
2022-10-13 03:08:46 -0700

    Add files via upload

--- A67.hpp
+++ A67.hpp
@@ -18,11 +18,9 @@ void swap(int &, int &, int &, int &);
 
 
 
-
-
 // Functions
 
-void getinput(int &val1, int &val2) {
+void getinput(int val1, int val2) {
     cout << "Enter Value 1: ";
     cin >> val1;
     cout << "Enter Value 2: ";
@@ -38,7 +36,7 @@ void swap(int &val1, int &val2) {
 
 }
 
-void getinput(int &val1, int &val2, int &val3) {
+void getinput(int val1, int val2, int val3) {
     cout << "Enter Value 1: ";
     cin >> val1;
     cout << "Enter Value 2: ";
@@ -53,24 +51,25 @@ void swap(int &val1, int &val2, int &val3) {
     sum = val1 + val2 + val3;
 
         if( (val1 > val2 && val1 > val3) )
-        val1 = max;
+        max = val1;
     else if (val2 > val3)
-        val2 = max;
+        max = val2;
     else
-        val3 = max;
+        max = val3;
 
     if ( (val1 < val2 && val1 < val3) )
-        val1 = min;
+        min = val1;
     else if (val2 < val3)
-        val2 = min;
+        min = val2;
     else 
-        val3 = min;
+        min = val3;
 
     remainder = (sum - max - min);
 
+
 }
 
-void getinput(int &val1, int &val2, int &val3, int &val4) {
+void getinput(int val1, int val2, int val3, int val4) {
     cout << "Enter Value 1: ";
     cin >> val1;
     cout << "Enter Value 2: ";
